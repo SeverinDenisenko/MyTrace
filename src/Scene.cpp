66a88d854f7288c45b6fd88a78e4ea000f19a4d7
severin-denisenko <devchonkavkorobchonke@gmail.com>
2023-03-28 08:33:46 +0300

    Refactor scene

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -9,22 +9,17 @@ void Scene::Add(const std::shared_ptr<Object>& object) {
 }
 
 class Hit Scene::Hit(const Ray& ray, double min, double max) const {
-    if (objects.empty())
-        return {};
-
     class Hit closestHit;
-    bool hitted = false;
 
     for (const auto & object : objects) {
         class Hit hit = object->Hit(ray, min, max);
 
         if (hit){
-            if (hitted){
+            if (closestHit){
                 if (hit.t < closestHit.t)
                     closestHit = hit;
             } else {
                 closestHit = hit;
-                hitted = true;
             }
         }
     }
